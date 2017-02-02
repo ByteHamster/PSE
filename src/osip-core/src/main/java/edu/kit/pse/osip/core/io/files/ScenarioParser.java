b3602edaf513ec0fbfa9158e2284ebac1b9de8a0
ByteHamster <info@bytehamster.com>
2017-02-02 23:20:32 +0100

    [core/io/files] Added parser for scenarios

--- src/osip-core/src/main/java/edu/kit/pse/osip/core/io/files/ScenarioParser.java
+++ src/osip-core/src/main/java/edu/kit/pse/osip/core/io/files/ScenarioParser.java
@@ -1,33 +1,173 @@
 package edu.kit.pse.osip.core.io.files;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.Consumer;
+
 import edu.kit.pse.osip.core.model.base.ProductionSite;
+import edu.kit.pse.osip.core.model.base.TankSelector;
 import edu.kit.pse.osip.core.model.behavior.Scenario;
 
 /**
  * Parser for OSIP scenarios
+ * @author Hans-Peter Lehmann
+ * @version 1.0
  */
 public class ScenarioParser extends edu.kit.pse.osip.core.io.files.ExtendedParser {
+    private HashMap<String, CommandAction> commands = new HashMap<>();    
+    protected Scenario scenario = new Scenario();
+    
     /**
      * Constructor of ScenarioParser
-     * @param toParse @param toParse String to parse
+     * @param toParse
+     * @param toParse String to parse
      */
     public ScenarioParser(String toParse) {
         super(toParse);
-        throw new RuntimeException("Not implemented!");
+        addCommands();
     }
+    
     /**
-     * Parse scenario @return parsed scenario
+     * Parse scenario
+     * @return parsed scenario
      * @throws ParserException If something goes wrong
      */
-    public final Scenario readScenario() {
-        throw new RuntimeException("Not implemented!");
+    public final Scenario readScenario() throws ParserException {
+        while (available()) {
+            int lastPosition = -1;
+            while (available() && lastPosition != currentPosition) {
+                lastPosition = currentPosition;
+                skipWhitespaces();
+                skipComments();
+            }
+            if (available()) {
+              readStatement();
+            }
+        }
+        return scenario;
     }
+    
     /**
-     * Reads a command in the input file
-     * @return The command 
+     * Reads a statement in the input file
      * @throws ParserException If something goes wrong
      */
-    public final java.util.function.Consumer<ProductionSite> readCommand() {
-        throw new RuntimeException("Not implemented!");
+    public final void readStatement() throws ParserException {
+        String command = readAlphabetic();
+        skipWhitespaces();
+        
+        if (command.equals("var")) {
+            // Variable assignment
+            String name = readAlphabetic();
+            skipWhitespaces();
+            check('=');
+            skipWhitespaces();
+            float value = readExpression();
+            skipWhitespaces();
+            check(';');
+            super.variables.put(name, value);
+        } else {
+            // Function call
+            ArrayList<Float> parameters = new ArrayList<>();
+            check('(');
+            while (true) {
+                skipWhitespaces();
+                parameters.add(readExpression());
+                skipWhitespaces();
+                if (peek() == ',') {
+                    check(',');
+                    skipWhitespaces();
+                } else {
+                    break;
+                }
+            }
+            skipWhitespaces();
+            check(')');
+            skipWhitespaces();
+            check(';');
+            
+            if (command.equals("delay")) {
+              checkArgumentCount(parameters, 1);
+              scenario.addPause(Math.round(parameters.get(0)));
+            } else if (!commands.containsKey(command)) {
+                die("Unknown command: " + command);
+            } else {
+                scenario.appendRunnable(commands.get(command).create(parameters));
+            }
+        }
+    }
+    
+    /**
+     * Reads an alphabetic string, must not be empty
+     * @return The read string
+     * @throws ParserException If something goes wrong
+     */
+    private String readAlphabetic() throws ParserException {
+        String name = "";
+        while (available() && Character.isAlphabetic(peek())) {
+            name += pop();
+        }
+        if (name.equals("")) {
+            die("Expected String literal");
+        }
+        return name;
+    }
+    
+    /**
+     * Requires a specific number of arguments to be available
+     * @param parameters Parameters given
+     * @param required Expected parameter count
+     * @throws ParserException If something goes wrong
+     */
+    private void checkArgumentCount(ArrayList<Float> parameters, int required) throws ParserException {
+        if (parameters.size() != required) {
+            die("Invalid number uf arguments");
+        }
+    }
+    
+    /**
+     * Create and add all supported commands
+     */
+    private void addCommands() {
+        commands.put("setMotorRpm", (parameters) -> {
+            checkArgumentCount(parameters, 1);
+            return (site) -> site.getMixTank().getMotor().setRPM(Math.round(parameters.get(0)));
+        });
+        commands.put("setUpperInThreshold", (parameters) -> {
+            checkArgumentCount(parameters, 2);
+            if (Math.round(parameters.get(1)) > 100 || Math.round(parameters.get(1)) < 0) {
+                die("Argument out of range");
+            }
+            return (site) -> site.getUpperTank(TankSelector.valuesWithoutMix()[Math.round(parameters.get(0))])
+                .getInPipe().setValveThreshold((byte) Math.round(parameters.get(1)));
+        });
+        commands.put("setUpperOutThreshold", (parameters) -> {
+            checkArgumentCount(parameters, 2);
+            if (Math.round(parameters.get(1)) > 100 || Math.round(parameters.get(1)) < 0) {
+                die("Argument out of range");
+            }
+            return (site) -> site.getUpperTank(TankSelector.valuesWithoutMix()[Math.round(parameters.get(0))])
+                .getOutPipe().setValveThreshold((byte) Math.round(parameters.get(1)));
+        });
+        commands.put("setMixOutThreshold", (parameters) -> {
+            checkArgumentCount(parameters, 1);
+            if (Math.round(parameters.get(0)) > 100 || Math.round(parameters.get(0)) < 0) {
+                die("Argument out of range");
+            }
+            return (site) -> site.getMixTank().getOutPipe().setValveThreshold((byte) Math.round(parameters.get(0)));
+        });
+    }
+    
+    /**
+     * Allows to call a specific command.
+     */
+    @FunctionalInterface
+    interface CommandAction {
+        /**
+         * Creates the Consumer that is needed by ProductionSite
+         * @param parameters Parameters of this function call
+         * @return The consumer
+         * @throws ParserException If the argument count does not match
+         */
+        Consumer<ProductionSite> create(ArrayList<Float> parameters) throws ParserException;
     }
 }
---
+++ src/osip-core/src/test/java/edu/kit/pse/osip/core/io/files/ScenarioParserTest.java
@@ -0,0 +1,92 @@
+package edu.kit.pse.osip.core.io.files;
+
+import org.junit.Test;
+
+/**
+ * Tests the extended parser class 
+ * @author Hans-Peter Lehmann
+ * @version 1.0
+ */
+public class ScenarioParserTest {
+    /**
+     * Tests if variables can be assigned
+     * @throws ParserException If something goes wrong
+     */
+    @Test
+    public void testVariableAssignments() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("var x = 15;");
+        parser.readStatement();
+        TestUtils.assertFloat(parser.variables.get("x"), 15f);
+        
+        parser = new ScenarioParser("var myVariable = -(3+3);");
+        parser.readStatement();
+        TestUtils.assertFloat(parser.variables.get("myVariable"), -6f);
+        
+        parser = new ScenarioParser("var var = -var;");
+        parser.variables.put("var", 42f);
+        parser.readStatement();
+        TestUtils.assertFloat(parser.variables.get("var"), -42f);
+    }
+    
+    /**
+     * Tests if an undefined variable can be read
+     * @throws ParserException If something goes wrong
+     */
+    @Test(expected = ParserException.class)
+    public void testUnknownVariableAssignment() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("var x = 15 + z;");
+        parser.readStatement();
+    }
+    
+    /**
+     * Tests if invalid assignments results in errors
+     * @throws ParserException If something goes wrong
+     */
+    @Test(expected = ParserException.class)
+    public void testInvalidAssignmentSyntax() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("var x = 10 = 5;");
+        parser.readStatement();
+    }
+    
+    /**
+     * Tests if multiple variables can be assigned
+     * @throws ParserException If something goes wrong
+     */
+    @Test
+    public void testMultipleVariableAssignments() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("var x = 15;var y = 16;");
+        parser.readScenario();
+        TestUtils.assertFloat(parser.variables.get("x"), 15f);
+        TestUtils.assertFloat(parser.variables.get("y"), 16f);
+        
+        parser = new ScenarioParser("var x = 15;var y = x + 3;");
+        parser.readScenario();
+        TestUtils.assertFloat(parser.variables.get("x"), 15f);
+        TestUtils.assertFloat(parser.variables.get("y"), 18f);
+    }
+    
+    /**
+     * Tests if comments and assignments can be mixed
+     * @throws ParserException If something goes wrong
+     */
+    @Test
+    public void testComments() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("# Comment\n"
+            + "var x = 5; # Assigns 5 to x\n"
+            + "\n\n"
+            + "var y = 10;");
+        parser.readScenario();
+        TestUtils.assertFloat(parser.variables.get("x"), 5f);
+        TestUtils.assertFloat(parser.variables.get("y"), 10f);
+    }
+    
+    /**
+     * Tests if wrong argument count leads to errors
+     * @throws ParserException If something goes wrong
+     */
+    @Test(expected = ParserException.class)
+    public void testCommandArgumentCount() throws ParserException {
+        ScenarioParser parser = new ScenarioParser("delay(5, 10);");
+        parser.readScenario();
+    }
+}
