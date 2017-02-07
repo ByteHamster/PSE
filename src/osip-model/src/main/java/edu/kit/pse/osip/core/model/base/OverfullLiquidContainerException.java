1bd22e4b029ea978766d12abb7e5cacfd0d58260
David Kahles <david.kahles96@gmail.com>
2017-02-07 11:24:59 +0100

    [core/model/base] Implement OverfullLiquidContainerException

--- src/osip-model/src/main/java/edu/kit/pse/osip/core/model/base/OverfullLiquidContainerException.java
+++ src/osip-model/src/main/java/edu/kit/pse/osip/core/model/base/OverfullLiquidContainerException.java
@@ -5,6 +5,9 @@ package edu.kit.pse.osip.core.model.base;
  */
 public class OverfullLiquidContainerException extends java.lang.IllegalArgumentException {
     private static final long serialVersionUID = 9182764382384359500L;
+    private final float maximumAmount;
+    private final float triedAmount;
+
     /**
      * Create a new OverfullLiquidContainerException
      * @throws IllegalArgumentException if maximumAmount or triedAmount is < 0.
@@ -12,21 +15,25 @@ public class OverfullLiquidContainerException extends java.lang.IllegalArgumentE
      * @param maximumAmount How much one can insert at maximum.
      * @param triedAmount How much was tried to be inserted. This should be more than maximumAmount.
      */
-    public OverfullLiquidContainerException (java.lang.String message, float maximumAmount, float triedAmount) {
-        throw new RuntimeException("Not implemented!");
+    public OverfullLiquidContainerException(String message, float maximumAmount, float triedAmount) {
+        super(message);
+        this.maximumAmount = maximumAmount;
+        this.triedAmount = triedAmount;
     }
+
     /**
      * Returns the maximum amount
      * @return the maximum amount.
      */
-    public final float getMaximumAmount () {
-        throw new RuntimeException("Not implemented!");
+    public final float getMaximumAmount() {
+        return maximumAmount;
     }
+
     /**
      * Returns the amount that was tried to input.
      * @return the tried amount.
      */
-    public final float getTriedAmount () {
-        throw new RuntimeException("Not implemented!");
+    public final float getTriedAmount() {
+        return triedAmount;
     }
 }
