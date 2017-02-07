3623e612078ccc939bd7dd7fbcfe567429b3db81
ByteHamster <info@bytehamster.com>
2017-02-07 23:32:10 +0100

    [core/model/simulation] Added simulation production site

--- src/osip-model-simulation/src/main/java/edu/kit/pse/osip/core/model/simulation/ProductionSiteSimulation.java
+++ src/osip-model-simulation/src/main/java/edu/kit/pse/osip/core/model/simulation/ProductionSiteSimulation.java
@@ -3,7 +3,11 @@ package edu.kit.pse.osip.core.model.simulation;
 import edu.kit.pse.osip.core.model.base.TankSelector;
 
 /**
- * This is a ProductionSite which returns the simulated tanks of the package "simulation" instead of the non-simulated tanks of package "base".
+ * This is a ProductionSite which returns the simulated tanks of the package "simulation"
+ * instead of the non-simulated tanks of package "base".
+ *
+ * @author Hans-Peter Lehmann
+ * @version 1.0
  */
 public class ProductionSiteSimulation extends edu.kit.pse.osip.core.model.base.ProductionSite {
     /**
@@ -11,14 +15,14 @@ public class ProductionSiteSimulation extends edu.kit.pse.osip.core.model.base.P
      * @return the requested tank that is simulatable.
      * @param tank Specifies the tank.
      */
-    public final TankSimulation getUpperTank (TankSelector tank) {
+    public TankSimulation getUpperTank(TankSelector tank) {
         throw new RuntimeException("Not implemented!");
     }
     /**
      * Get the mixtank which is extended to be simulatable.
      * @return the requested mixtank that is simulatable.
      */
-    public final MixTankSimulation getMixTank () {
+    public MixTankSimulation getMixTank() {
         throw new RuntimeException("Not implemented!");
     }
 }
