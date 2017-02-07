c38b0b0c7a4add845a028b5e528cfe550010bf70
David Kahles <david.kahles96@gmail.com>
2017-02-07 11:24:59 +0100

    [core/model/base] Implement OverfullLiquidContainerException

---
+++ src/osip-model/src/test/java/edu/kit/pse/osip/core/model/base/OverfullLiquidContainerExceptionTest.java
@@ -0,0 +1,24 @@
+package edu.kit.pse.osip.core.model.base;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A class to test the OverfullLiquidContainerException class.
+ *
+ * @author David Kahles
+ * @version 1.0
+ */
+public class OverfullLiquidContainerExceptionTest {
+    /**
+     * Check whether the getters work as expected.
+     */
+    @Test
+    public void testGetter() {
+        OverfullLiquidContainerException ex = new OverfullLiquidContainerException("test", 1, 2);
+        assertEquals(1, ex.getMaximumAmount(), 0.001);
+        assertEquals(1, ex.getMaximumAmount(), 0.001);
+        assertEquals("test", ex.getMessage());
+    }
+}
