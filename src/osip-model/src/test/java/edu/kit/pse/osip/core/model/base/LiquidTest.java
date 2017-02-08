b74031f700df109055396cee4b6fa02a24a360cb
David Kahles <david.kahles96@gmail.com>
2017-02-08 13:58:29 +0100

    [core/mode/base] Adapt multiple classes to SimulationConstants class

--- src/osip-model/src/test/java/edu/kit/pse/osip/core/model/base/LiquidTest.java
+++ src/osip-model/src/test/java/edu/kit/pse/osip/core/model/base/LiquidTest.java
@@ -1,5 +1,6 @@
 package edu.kit.pse.osip.core.model.base;
 
+import edu.kit.pse.osip.core.SimulationConstants;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,11 +16,11 @@ import static org.junit.Assert.assertTrue;
  */
 public class LiquidTest {
     /**
-     * Test whether the constructor accepts a negative temperature.
+     * Test whether the constructor accepts too low temperatures
      */
     @Test(expected = IllegalArgumentException.class)
     public void testNegativeTemperature() {
-        new Liquid(1, -0.1f, new Color(0, 0, 0));
+        new Liquid(1, SimulationConstants.MIN_TEMPERATURE - 0.01f, new Color(0, 0, 0));
     }
 
     /**
@@ -27,7 +28,7 @@ public class LiquidTest {
      */
     @Test(expected = IllegalArgumentException.class)
     public void testNegativeAmount() {
-        new Liquid(-1.1f, 5, new Color(0, 0, 0));
+        new Liquid(-1.1f, SimulationConstants.MIN_TEMPERATURE, new Color(0, 0, 0));
     }
 
     /**
@@ -35,7 +36,7 @@ public class LiquidTest {
      */
     @Test(expected = IllegalArgumentException.class)
     public void testNullColor() {
-        new Liquid(1, 5, null);
+        new Liquid(1, SimulationConstants.MIN_TEMPERATURE, null);
     }
 
     /**
@@ -43,9 +44,9 @@ public class LiquidTest {
      */
     @Test
     public void testGetter() {
-        Liquid liquid = new Liquid(0, 1, new Color(0, 0, 0));
+        Liquid liquid = new Liquid(0, SimulationConstants.MIN_TEMPERATURE, new Color(0, 0, 0));
         assertEquals(0, liquid.getAmount(), 0.001);
-        assertEquals(1, liquid.getTemperature(), 0.001);
+        assertEquals(SimulationConstants.MIN_TEMPERATURE, liquid.getTemperature(), 0.001);
         assertEquals(new Color(0, 0, 0), liquid.getColor());
     }
 
@@ -54,10 +55,14 @@ public class LiquidTest {
      */
     @Test
     public void testHashcode() {
-        Liquid liquid1 = new Liquid(1, 1, new Color(1, 1, 1));
-        Liquid liquid2 = new Liquid(1, 1, new Color(1, 1, 1));
+        Liquid liquid1 = new Liquid(1, SimulationConstants.MIN_TEMPERATURE, new Color(1, 1, 1));
+        Liquid liquid2 = new Liquid(1, SimulationConstants.MIN_TEMPERATURE, new Color(1, 1, 1));
         assertEquals(liquid1.hashCode(), liquid2.hashCode());
-        liquid2 = new Liquid(1, 1, new Color(0, 1, 1));
+
+        liquid2 = new Liquid(2, SimulationConstants.MIN_TEMPERATURE, new Color(1, 1, 1));
+        assertNotEquals(liquid1.hashCode(), liquid2.hashCode());
+
+        liquid2 = new Liquid(1, SimulationConstants.MAX_TEMPERATURE, new Color(1, 1, 1));
         assertNotEquals(liquid1.hashCode(), liquid2.hashCode());
     }
 
@@ -67,13 +72,18 @@ public class LiquidTest {
     @Test
     public void testEquals() {
         Color color = new Color(1, 1, 1);
-        Liquid liquid1 = new Liquid(1, 1, color);
-        Liquid liquid2 = new Liquid(1, 1, color);
+        Liquid liquid1 = new Liquid(1, SimulationConstants.MIN_TEMPERATURE, color);
+        Liquid liquid2 = new Liquid(1, SimulationConstants.MIN_TEMPERATURE, color);
         assertTrue(liquid1.equals(liquid2));
         assertTrue(liquid2.equals(liquid1));
         assertFalse(liquid1.equals(null));
         assertFalse(liquid1.equals(new String("test")));
-        liquid2 = new Liquid(2, 1, color);
+
+        liquid2 = new Liquid(2, SimulationConstants.MIN_TEMPERATURE, color);
+        assertFalse(liquid1.equals(liquid2));
+        assertFalse(liquid2.equals(liquid1));
+
+        liquid2 = new Liquid(1, SimulationConstants.MAX_TEMPERATURE, color);
         assertFalse(liquid1.equals(liquid2));
         assertFalse(liquid2.equals(liquid1));
     }
