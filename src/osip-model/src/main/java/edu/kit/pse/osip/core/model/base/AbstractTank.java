1f61f50f6649fcde0df2fa5b54b5a3ea073799a7
David Kahles <david.kahles96@gmail.com>
2017-02-08 15:27:51 +0100

    [core/model/base] Implement AbstractTank, without tests

--- src/osip-model/src/main/java/edu/kit/pse/osip/core/model/base/AbstractTank.java
+++ src/osip-model/src/main/java/edu/kit/pse/osip/core/model/base/AbstractTank.java
@@ -1,53 +1,81 @@
 package edu.kit.pse.osip.core.model.base;
 
 /**
- * The model of a tank. It is the base class of the mixtank and the upper tanks, so it has some shared functionality which is avaiable in the mixtank and the upper tanks.
+ * The model of a tank. It is the base class of the mixtank and the upper tanks, so it has some shared functionality
+ * which is avaiable in the mixtank and the upper tanks.
+ * @author David Kahles
+ * @version 1.0
  */
 public abstract class AbstractTank extends java.util.Observable {
-    public Liquid liquid;
+    private Liquid liquid;
+    private float capacity;
+    private TankSelector selector;
+    private Pipe outPipe;
+
     /**
      * Create a new tank.
      * @param capacity The size of the tank in cm³.
-     * @param tankSelector Specifies which tank it is. This is useful if you have access to the tank and need to get a unique identifier.
+     * @param tankSelector Specifies which tank it is. This is useful if you have access to the tank and need to get
+     *                     a unique identifier.
      * @param liquid The default liquid of the tank.
      * @param outPipe The outgoing pipe.
      */
-    public final void Tank (float capacity, TankSelector tankSelector, Liquid liquid, Pipe outPipe) {
-        throw new RuntimeException("Not implemented!");
+    public AbstractTank(float capacity, TankSelector tankSelector, Liquid liquid, Pipe outPipe) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity of a tank needs to be greather than zero");
+        }
+        if (tankSelector == null || liquid == null || outPipe == null) {
+            throw new NullPointerException("Arguments of AbstractTank must not be null");
+        }
+        this.capacity = capacity;
+        this.selector = tankSelector;
+        this.liquid = liquid;
+        this.outPipe = outPipe;
     }
+
     /**
-     * Get the fill level of the tank in %.
-     * @return the fill level in %.
+     * Get the fill level of the tank in % between 0 and 1 (or more than 1, if the tank is overfull).
+     * @return the fill level in % between 0 and 1.
      */
-    public final float getFillLevel () {
-        throw new RuntimeException("Not implemented!");
+    public float getFillLevel() {
+        return liquid.getAmount() / capacity;
     }
+
     /**
-     * Set the liquid of the tank. It doesn't throw an exception if you put in more liquid than possible, so you can detect overflow conditions.
+     * Set the liquid of the tank. It doesn't throw an exception if you put in more liquid than possible, so you can
+     * detect overflow conditions.
      * @param liquid The liquid which should be in the tank afterwards.
      */
-    public final void setLiquid (Liquid liquid) {
-        throw new RuntimeException("Not implemented!");
+    public synchronized void setLiquid(Liquid liquid) {
+        if (liquid == null) {
+            throw new NullPointerException("The liquid of a tank must not be null");
+        }
+        setChanged();
+        this.liquid = liquid;
+        notifyObservers(liquid);
     }
+
     /**
      * Get the tank selector. This is useful if you have access to the tank and need to get a unique identifier.
      * @return the tank selecor.
      */
-    public final TankSelector getTankSelector () {
-        throw new RuntimeException("Not implemented!");
+    public TankSelector getTankSelector() {
+        return selector;
     }
+
     /**
      * Get the outgoing pipe of the tank.
      * @return the outgoing pipe.
      */
-    public final Pipe getOutPipe () {
-        throw new RuntimeException("Not implemented!");
+    public Pipe getOutPipe() {
+        return outPipe;
     }
+
     /**
      * Get the liquid of the tank.
      * @return the liquid of the tank.
      */
-    public final Liquid getLiquid () {
-        throw new RuntimeException("Not implemented!");
+    public Liquid getLiquid() {
+        return liquid;
     }
 }
