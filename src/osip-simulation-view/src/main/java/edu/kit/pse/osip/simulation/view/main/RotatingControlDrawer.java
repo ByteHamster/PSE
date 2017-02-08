1e9ea8deb314e16c85314b48c6b332b67dead40a
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-08 01:26:43 +0100

    [simulation.view] Implement RotatingControlDrawer.

--- src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/main/RotatingControlDrawer.java
+++ src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/main/RotatingControlDrawer.java
@@ -4,6 +4,10 @@ package edu.kit.pse.osip.simulation.view.main;
  * The parent for all rotating gui elements. It needs a position and a rotational speed. It provides private methods for rotation of the shapes making up the visualization, depending on the given speed.
  */
 public abstract class RotatingControlDrawer extends edu.kit.pse.osip.simulation.view.main.ObjectDrawer {
+
+    private Point2D position;
+    private int speed;
+
     /**
      * Sets the position by using super(pos) and the rotation speed.
      * @param pos The upper left corner
@@ -11,6 +15,7 @@ public abstract class RotatingControlDrawer extends edu.kit.pse.osip.simulation.
      */
     public RotatingControlDrawer (Point2D pos, int speed) {
         super(pos);
-        throw new RuntimeException("Not implemented!");
+        this.position = pos;
+        this.speed = speed;
     }
 }
