6da99ca0622e8a29b35cafa6bbe1066fd1cff4e1
marimbMax <stick@nurfuerspam.de>
2017-02-09 21:42:42 +0100

    [core/io/networking] Added RemoteMachine class and it's unit test class.

--- src/osip-core/src/main/java/edu/kit/pse/osip/core/io/networking/RemoteMachine.java
+++ src/osip-core/src/main/java/edu/kit/pse/osip/core/io/networking/RemoteMachine.java
@@ -2,26 +2,33 @@ package edu.kit.pse.osip.core.io.networking;
 
 /**
  * Class for saving connection and networking details
+ * @author  Maximilian Schwarzmann
+ * @version 1.0
  */
 public class RemoteMachine {
+    
+    private String hostname;
+    private int port;
+    
     /**
      * Constructor of RemoteMachine
      * @param hostname host name of RemoteMachine
      * @param port The port of the RemoteMachine
      */
-    public RemoteMachine (String hostname, int port) {
-        throw new RuntimeException("Not implemented!");
+    public RemoteMachine(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
     }
     /**
      * Getter method of the hostname @return hostname of RemoteMachine
      */
-    public final String getHostname () {
-        throw new RuntimeException("Not implemented!");
+    public final String getHostname() {
+        return hostname;
     }
     /**
      * Getter method of port @return port of RemoteMachine
      */
-    public final int getPort () {
-        throw new RuntimeException("Not implemented!");
+    public final int getPort() {
+        return port;
     }
 }
