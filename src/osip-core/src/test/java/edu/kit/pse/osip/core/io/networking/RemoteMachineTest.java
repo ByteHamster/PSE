7f101b4d1a9c3dce09b5f2b2ee23f495bc2c799b
marimbMax <stick@nurfuerspam.de>
2017-02-09 23:12:21 +0100

    [core/io/networking] Added RemoteMachine class and its unit test class

---
+++ src/osip-core/src/test/java/edu/kit/pse/osip/core/io/networking/RemoteMachineTest.java
@@ -0,0 +1,31 @@
+package edu.kit.pse.osip.core.io.networking;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test class for RemoteMachine
+ * @author Maximilian Schwarzmann
+ * @version 1.0
+ */
+public class RemoteMachineTest {
+
+    /**
+     * Tests getter of hostname
+     */
+    @Test
+    public void testHostnameGetter() {
+        RemoteMachine machine = new RemoteMachine("mac", 10);
+        assertEquals("mac", machine.getHostname());
+    }
+
+    /**
+     * Tests getter of port
+     */
+    @Test
+    public void testPortGetter() {
+        RemoteMachine machine = new RemoteMachine("mac", 10);
+        assertEquals(10, machine.getPort());
+    }    
+}
