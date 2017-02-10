27dabb49a13dfb1ccacb07f97d907121d484af0d
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-10 17:42:16 +0100

    [monitor-dashboard] Implemented the TankVisualization.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/TankVisualization.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/TankVisualization.java
@@ -1,20 +1,36 @@
 package edu.kit.pse.osip.monitoring.view.dashboard;
 
 import edu.kit.pse.osip.core.model.base.Tank;
+import edu.kit.pse.osip.core.utils.language.Translator;
+import javafx.scene.paint.Color;
 
 /**
  * Visualization for a normal tank.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class TankVisualization extends edu.kit.pse.osip.monitoring.view.dashboard.AbstractTankVisualization {
+final class TankVisualization extends AbstractTankVisualization {
     /**
      * Visualization of the supply pipe.
      */
     private GaugeVisualization supply;
+    
     /**
      * Creates a new visualization.
+     * 
      * @param tank The tank to display
      */
-    protected TankVisualization (Tank tank) {
-        throw new RuntimeException("Not implemented!");
+    protected TankVisualization(Tank tank) {
+        super(tank);
+        supply = new GaugeVisualization(Translator.getInstance().getString("monitoring.tank.supply"), tank.getInPipe());
+        this.add(supply, 0, 4);
+        this.add(drain, 1, 4);
+        this.add(fillLevel, 0, 5);
+        this.add(temperature, 1, 5);
+        this.add(progresses, 0, 6, 2, 1);        
+        edu.kit.pse.osip.core.model.base.Color currentColor = tank.getLiquid().getColor();
+        Color convertedColor = new Color(currentColor.getR(), currentColor.getG(), currentColor.getB(), 1.0);
+        this.setBorderColor(convertedColor);
     }
 }
