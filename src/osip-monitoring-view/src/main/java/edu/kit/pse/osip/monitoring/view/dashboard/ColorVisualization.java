3508c14fd7e6001fe551c00d352c9e03a97502d1
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-10 12:59:56 +0100

    [monitor-dashboard] Implemented the ColorVisualization.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/ColorVisualization.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/ColorVisualization.java
@@ -1,43 +1,51 @@
 package edu.kit.pse.osip.monitoring.view.dashboard;
 
+import edu.kit.pse.osip.core.model.base.AbstractTank;
+import edu.kit.pse.osip.core.utils.language.Translator;
+import java.util.Observable;
+import java.util.Observer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 /**
- * Visualises the color of the mixtank.
+ * Visualizes the color of the mixtank.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class ColorVisualization implements java.util.Observer {
-    /**
-     * Label for showing the color section.
-     */
-    private javafx.scene.control.Label colorLabel;
+final class ColorVisualization extends BarLayout implements Observer {
     /**
      * Rectangle contains the color.
      */
-    private javafx.scene.shape.Rectangle colorRectangle;
+    private Rectangle colorRectangle;
+    
     /**
      * Creates and initializes all controls for the color visualization.
      */
-    protected ColorVisualization () {
-        throw new RuntimeException("Not implemented!");
+    protected ColorVisualization() {
+        super(Translator.getInstance().getString("monitoring.mixTank.color"));
+        colorRectangle = new Rectangle();
+        this.getChildren().add(colorRectangle);
     }
-    /**
-     * Returns the label showing the color section.
-     * @return the label showing the color section.
-     */
-    protected final javafx.scene.control.Label getColorLabel () {
-        throw new RuntimeException("Not implemented!");
+    
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+        double length = this.getWidth() / 2.0;
+        colorRectangle.setLayoutX(length / 2);
+        colorRectangle.setWidth(length);
+        colorRectangle.setHeight(length);
     }
-    /**
-     * Returns the rectangle containing the current color.
-     * @return the rectangle containing the current color.
-     */
-    protected final javafx.scene.shape.Rectangle getColorRectangle () {
-        throw new RuntimeException("Not implemented!");
-    }
+
     /**
      * Called when the observed object changed.
+     * 
      * @param observable The observed object.
      * @param object The new value.
      */
-    public final void update (java.util.Observable observable, Object object) {
-        throw new RuntimeException("Not implemented!");
+    public void update(Observable observable, Object object) {
+        edu.kit.pse.osip.core.model.base.Color col = ((AbstractTank) observable).getLiquid().getColor();
+        Color currentColor = new Color(col.getR(), col.getG(), col.getB(), 1.0);
+        colorRectangle.setFill(currentColor);
     }
 }
