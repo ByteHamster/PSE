b844161c48793218fcd48186a0922c3af278d38c
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-10 18:19:46 +0100

    [monitor-dashboard] Implemented the MainClass and parts of the MonitoringController. Also, removed some unnecessary files.

--- src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MainClass.java
+++ src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MainClass.java
@@ -2,19 +2,23 @@ package edu.kit.pse.osip.monitoring.controller;
 
 /**
  * The main class as entry point for the whole monitoring.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class MainClass {
+public final class MainClass {
     /**
      * Private constructor to avoid instantiation of the main class.
      */
-    private MainClass () {
-        throw new RuntimeException("Not implemented!");
+    private MainClass() {
     }
+    
     /**
-     * Main entry point into the jar
+     * Main entry point into the jar.
+     * 
      * @param args Command line arguments.
      */
-    public final static void main (String[] args) {
-        throw new RuntimeException("Not implemented!");
+    public static void main(String[] args) {
+        MonitoringController.launch(MonitoringController.class, args);
     }
 }
--- src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MonitoringController.java
+++ src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MonitoringController.java
@@ -2,53 +2,84 @@ package edu.kit.pse.osip.monitoring.controller;
 
 import edu.kit.pse.osip.core.io.files.ClientSettingsWrapper;
 import edu.kit.pse.osip.core.model.base.ProductionSite;
+import edu.kit.pse.osip.monitoring.view.dashboard.MonitoringViewFacade;
+import edu.kit.pse.osip.monitoring.view.settings.SettingsViewFacade;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
- * The controller starts the monitoring view and initializes the model with the main loop. It is also responsible for the communication between the view and model.
+ * The controller starts the monitoring view and initializes the model with the main loop.
+ * It is also responsible for the communication between the view and model.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class MonitoringController extends javafx.application.Application {
+public final class MonitoringController extends Application {
     /**
      * The current model.
      */
     private ProductionSite currentProductionSite;
+    
     /**
      * Contains the current settings.
      */
     private ClientSettingsWrapper currentSettings;
+    
     /**
      * The current entry point to the monitoring view.
      */
     private MonitoringViewInterface currentView;
+    
+    /**
+     * The current entry point for the settings view.
+     */
+    private SettingsViewInterface currentSettingsView;
+    
     /**
      * Creates a new controller instance.
      */
-    protected final void Controller () {
-        throw new RuntimeException("Not implemented!");
+    public MonitoringController() {
+        currentProductionSite = new ProductionSite();
+        currentSettings = new ClientSettingsWrapper();
+        currentView = new MonitoringViewFacade();
+        currentSettingsView = new SettingsViewFacade();
     }
+    
     /**
      * Initializes all necessary objects.
      */
-    public final void init () {
+    public void init() {
         throw new RuntimeException("Not implemented!");
     }
+    
     /**
      * Called when JavaFX creates and starts the application.
+     * 
      * @param primaryStage The stage used for displaying all controls.
      */
-    public final void start (javafx.stage.Stage primaryStage) {
-        throw new RuntimeException("Not implemented!");
+    public void start(Stage primaryStage) {
+        setupUI(primaryStage);
     }
+    
+    /**
+     * Initializes the UI.
+     * 
+     * @param stage The JavaFX stage.
+     */
+    private void setupUI(Stage stage) {
+        currentView.showMonitoringView(stage, currentProductionSite);
+        currentView.setMenuSettingsButtonHandler(new MenuSettingsButtonHandler(currentSettings, currentSettingsView));
+        currentView.setMenuHelpButtonHandler(new MenuHelpButtonHandler());
+        currentView.setMenuAboutButtonHandler(new MenuAboutButtonHandler());
+        currentSettingsView.setSettingsCancelButtonHandler(new SettingsCancelButtonHandler(currentSettingsView));
+        currentSettingsView.setSettingsSaveButtonHandler(new SettingsSaveButtonHandler(currentSettings,
+                currentView, currentSettingsView));
+    }    
+    
     /**
      * Called when the last window is closed.
      */
-    public final void stop () {
-        throw new RuntimeException("Not implemented!");
-    }
-    /**
-     * Initialize the UI
-     * @param stage The javafx stage
-     */
-    private final void setupUI (javafx.stage.Stage stage) {
+    public void stop() {
         throw new RuntimeException("Not implemented!");
     }
 }
