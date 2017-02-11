158fb18e283f8520dfa5ade62c7acc11025d7df7
marimbMax <stick@nurfuerspam.de>
2017-02-11 19:58:38 +0100

    [core/io/files] Added more tests in ServerSettingsWrapperTest

--- src/osip-core/src/main/java/edu/kit/pse/osip/core/io/files/ServerSettingsWrapper.java
+++ src/osip-core/src/main/java/edu/kit/pse/osip/core/io/files/ServerSettingsWrapper.java
@@ -15,22 +15,22 @@ import edu.kit.pse.osip.core.model.base.TankSelector;
 public class ServerSettingsWrapper {
     
     private File propFile;
-    private Properties props;
+    private Properties properties;
     
     /**
      * Constructor of ServerSettingsWrapper
      * @param propFile The file to save the settings to
-     * @throws IOException 
-     * @throws FileNotFoundException
+     * @throws IOException  Exception in FileInputStream
+     * @throws FileNotFoundException Exception in file param
      */
-    public ServerSettingsWrapper(File propFile) throws IOException, FileNotFoundException {
+    public ServerSettingsWrapper(File propFile) throws IOException, FileNotFoundException  {
         if (propFile == null) {
             throw new NullPointerException();
         }
         this.propFile = propFile;
-        props = new Properties();
+        properties = new Properties();
         FileInputStream in = new FileInputStream(propFile);
-        props.load(in);
+        properties.load(in);
         in.close();
     }
     /**
@@ -41,24 +41,24 @@ public class ServerSettingsWrapper {
     public final void setServerPort(TankSelector tank, int port) {
         String portString = tank.name();
         String tankString = String.valueOf(port);
-        props.setProperty(portString, tankString);
+        properties.setProperty(portString, tankString);
     }
     /**
      * Getter method of server port @return The saved port
      * @param tank The tank to get the value for
      */
     public final int getServerPort(TankSelector tank) {
-        String port = props.getProperty(tank.name());
+        String port = properties.getProperty(tank.name());
         return Integer.parseInt(port);
     }
     /**
      * Saves settings in file
-     * @throws IOException
-     * @throws FileNotFoundException
+     * @throws IOException Exception in FileOutputStream
+     * @throws FileNotFoundException Exception in file param
      */
-    public final void saveSettings() throws FileNotFoundException, IOException{
+    public final void saveSettings() throws FileNotFoundException, IOException {
         FileOutputStream out = new FileOutputStream(propFile);
-        props.store(out, "");
+        properties.store(out, null);
         out.close();
     }
 }
--- src/osip-core/src/test/java/edu/kit/pse/osip/core/io/files/ServerSettingsWrapperTest.java
+++ src/osip-core/src/test/java/edu/kit/pse/osip/core/io/files/ServerSettingsWrapperTest.java
@@ -1,40 +1,64 @@
 package edu.kit.pse.osip.core.io.files;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.kit.pse.osip.core.model.base.TankSelector;
+
+/**
+ * Test class for ServerSettingsWrapper
+ * @author Maximilian Schwarzmann
+ * @version 1.0
+ *
+ */
 public class ServerSettingsWrapperTest {
 
+    private ServerSettingsWrapper wrapper;
+    
+    /**
+     * Set up 
+     * @throws Exception Exception in file loading
+     */
     @Before
     public void setUp() throws Exception {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("testServerSettings.properties");
+        File propFile = new File(url.getPath());
+        wrapper = new ServerSettingsWrapper(propFile);
     }
 
-    @Test
-    public void testServerSettingsWrapper() {
-        fail("Not yet implemented");
-    }
-
+    /**
+     * Test setting server ports
+     */
     @Test
     public void testSetServerPort() {
-        fail("Not yet implemented");
+        wrapper.setServerPort(TankSelector.MAGENTA, 1042);        
+        assertEquals(wrapper.getServerPort(TankSelector.MAGENTA), 1042);
     }
 
+    /**
+     * Test getting server ports
+     */
     @Test
     public void testGetServerPort() {
-        fail("Not yet implemented");
+        int result = wrapper.getServerPort(TankSelector.MAGENTA);
+        assertEquals(result, 1000);
     }
 
+    /**
+     * test writing to file
+     * @throws FileNotFoundException File is not found
+     * @throws IOException Exception in IO streams
+     */
     @Test
-    public void testSaveSettings() {
-        fail("Not yet implemented");
+    public void testSaveSettings() throws FileNotFoundException, IOException {
+        wrapper.setServerPort(TankSelector.YELLOW, 1122);
+        wrapper.saveSettings();
     }
-    
-    @After
-    public void tearDown() {
-        
-    }
-
 }
