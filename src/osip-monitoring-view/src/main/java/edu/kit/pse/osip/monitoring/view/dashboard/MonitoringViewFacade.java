259be39dd6d45ef6e089bd0affaa1c471ce16501
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-11 01:29:21 +0100

    [monitor-dashboard] Implemented the MonitoringViewFacade.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/MonitoringViewFacade.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/MonitoringViewFacade.java
@@ -1,89 +1,119 @@
 package edu.kit.pse.osip.monitoring.view.dashboard;
 
+import edu.kit.pse.osip.core.model.base.ProductionSite;
 import edu.kit.pse.osip.core.model.base.TankSelector;
 import edu.kit.pse.osip.monitoring.controller.MenuAboutButtonHandler;
 import edu.kit.pse.osip.monitoring.controller.MenuHelpButtonHandler;
 import edu.kit.pse.osip.monitoring.controller.MenuSettingsButtonHandler;
+import edu.kit.pse.osip.monitoring.controller.MonitoringViewInterface;
+import javafx.stage.Stage;
 
 /**
  * Provides one single access point to the monitoring view.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class MonitoringViewFacade implements edu.kit.pse.osip.monitoring.controller.MonitoringViewInterface {
-    public MonitoringMainWindow mainWindow;
+public final class MonitoringViewFacade implements MonitoringViewInterface {
+    /**
+     * Saves the current used main window. All method invocations are directed to this object.
+     */
+    private MonitoringMainWindow mainWindow;
+    
     /**
      * Shows the monitoring view to the user.
+     * 
      * @param stage The stage used for displaying the controls.
+     * @param currentModel The current model used to initialize the observable objects and all initial values.
      */
-    public final void showMonitoringView (javafx.stage.Stage stage) {
-        throw new RuntimeException("Not implemented!");
+    public void showMonitoringView(Stage stage, ProductionSite currentModel) {
+        mainWindow = new MonitoringMainWindow(stage, currentModel);
     }
+    
     /**
      * Enables or disables the underflow alarm for a specified tank.
+     * 
      * @param tank The tank whose underflow alarm will be enabled or disabled.
      * @param alarmEnabled true if the alarm should be enabled and false otherwise.
      */
-    public final void setUnderflowAlarmEnabled (TankSelector tank, boolean alarmEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public void setUnderflowAlarmEnabled(TankSelector tank, boolean alarmEnabled) {
+        mainWindow.getTank(tank).setUnderflowAlarmEnabled(alarmEnabled);
     }
+    
     /**
      * Enables or disables the overflow alarm for a specified tank.
+     * 
      * @param tank The tank whose overflow alarm will be enabled or disabled.
      * @param alarmEnabled true if the alarm should be enabled and false otherwise.
      */
-    public final void setOverflowAlarmEnabled (TankSelector tank, boolean alarmEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public void setOverflowAlarmEnabled(TankSelector tank, boolean alarmEnabled) {
+        mainWindow.getTank(tank).setOverflowAlarmEnabled(alarmEnabled);
     }
+    
     /**
      * Enables or disables the alarm when the temperature becomes too high for a specified tank.
+     * 
      * @param tank The tank which alarm will be enabled or disabled.
      * @param alarmEnabled true when the alarm should be enabled. false otherwise.
      */
-    public final void setTemperatureOverheatingAlarmEnabled (TankSelector tank, boolean alarmEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public final void setTemperatureOverheatingAlarmEnabled(TankSelector tank, boolean alarmEnabled) {
+        mainWindow.getTank(tank).setTemperatureOverheatingAlarmEnabled(alarmEnabled);
     }
+    
     /**
      * Enables or disables the temperature alarm when it becomes too low for a specified tank.
+     * 
      * @param tank The tank which alarm will be turned on or off.
      * @param alarmEnabled true when the alarm should be enabled. false otherwise.
      */
-    public final void setTemperatureUndercoolingAlarmEnabled (TankSelector tank, boolean alarmEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public void setTemperatureUndercoolingAlarmEnabled(TankSelector tank, boolean alarmEnabled) {
+        mainWindow.getTank(tank).setTemperatureUndercoolingAlarmEnabled(alarmEnabled);
     }
+    
     /**
      * Enables or disables the logging of a fill level progression for a specified tank.
+     * 
      * @param tank The tank whose fill level progression is logged or not.
      * @param progressionEnabled true if the fill level progression should be logged and false otherwise.
      */
-    public final void setFillLevelProgressionEnabled (TankSelector tank, boolean progressionEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public void setFillLevelProgressionEnabled(TankSelector tank, boolean progressionEnabled) {
+        mainWindow.getTank(tank).setFillLevelProgressionEnabled(progressionEnabled);
     }
+    
     /**
      * Enables or disables the logging of the temperature progression of a specified tank.
+     * 
      * @param tank The tank whose temperature progression is logged or not.
      * @param progressionEnabled true if the temperature progression should be logged and false otherwise.
      */
-    public final void setTemperatureProgressionEnabled (TankSelector tank, boolean progressionEnabled) {
-        throw new RuntimeException("Not implemented!");
+    public void setTemperatureProgressionEnabled(TankSelector tank, boolean progressionEnabled) {
+        mainWindow.getTank(tank).setTemperatureProgressionEnabled(progressionEnabled);
     }
+    
     /**
      * Sets the handler for the settings menu button.
+     * 
      * @param handler The handler for the settings menu button.
      */
-    public final void setMenuSettingsButtonHandler (MenuSettingsButtonHandler handler) {
-        throw new RuntimeException("Not implemented!");
+    public void setMenuSettingsButtonHandler(MenuSettingsButtonHandler handler) {
+        mainWindow.getMonitoringMenu().setMenuSettingsButtonHandler(handler);
     }
+    
     /**
      * Sets the handler for the about menu button.
+     * 
      * @param handler The handler for the about menu button.
      */
-    public final void setMenuAboutButtonHandler (MenuAboutButtonHandler handler) {
-        throw new RuntimeException("Not implemented!");
+    public void setMenuAboutButtonHandler(MenuAboutButtonHandler handler) {
+        mainWindow.getMonitoringMenu().setMenuAboutButtonHandler(handler);
     }
+    
     /**
      * Sets the handler for the help menu button.
+     * 
      * @param handler The handler handles a click on the help menu button.
      */
-    public final void setMenuHelpButtonHandler (MenuHelpButtonHandler handler) {
-        throw new RuntimeException("Not implemented!");
+    public void setMenuHelpButtonHandler(MenuHelpButtonHandler handler) {
+        mainWindow.getMonitoringMenu().setMenuHelpButtonHandler(handler);
     }
 }
