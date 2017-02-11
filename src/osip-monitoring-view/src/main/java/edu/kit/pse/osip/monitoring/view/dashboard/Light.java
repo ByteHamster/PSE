ad7fe1ee29a8a079d2ed955198fa3c873998b114
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-11 01:24:08 +0100

    [monitor-dashboard] Implemented the Light.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/Light.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/Light.java
@@ -1,40 +1,133 @@
 package edu.kit.pse.osip.monitoring.view.dashboard;
 
+import edu.kit.pse.osip.core.model.behavior.TankAlarm;
+import java.util.Observable;
+import java.util.Observer;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 /**
  * Visualizes a traffic light.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class Light extends javafx.scene.layout.Pane implements java.util.Observer {
+final class Light extends VBox implements Observer {
     /**
      * Stores the current state of all alarms: The light is enabled when at least one other alarm is enabled.
      */
     private AlarmState state = AlarmState.ALARM_ENABLED;
+    
+    /**
+     * Counter for all triggered alarms. If it's above zero, at least one alarm is triggered.
+     */
+    private int triggeredAlarms;
+    
     /**
      * The circle for the red light.
      */
     private javafx.scene.shape.Circle redLight;
+    
     /**
      * The circle for the green light.
      */
     private javafx.scene.shape.Circle greenLight;
+    
     /**
      * Creates a new light.
      */
-    protected Light () {
-        throw new RuntimeException("Not implemented!");
+    protected Light() {
+        triggeredAlarms = 0;
+        redLight = new Circle();
+        redLight.setStrokeWidth(MonitoringViewConstants.STROKE_WIDTH);
+        this.getChildren().add(redLight);
+        greenLight = new Circle();
+        greenLight.setStrokeWidth(MonitoringViewConstants.STROKE_WIDTH);
+        this.getChildren().add(greenLight);
+        state = AlarmState.ALARM_ENABLED;
+        enableWithColor();
     }
+    
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+        double width = this.getWidth();
+        redLight.setLayoutX(width / 2);
+        redLight.setLayoutY(width / 2);
+        redLight.setRadius(width / 2);
+        greenLight.setLayoutX(width / 2);
+        greenLight.setLayoutY(1.5 * width + MonitoringViewConstants.ELEMENTS_GAP);
+        greenLight.setRadius(width / 2);
+    }
+    
     /**
      * Sets the alarm state.
+     * 
      * @param newState The new alarm state.
      */
-    protected final void setAlarmState (AlarmState newState) {
-        throw new RuntimeException("Not implemented!");
+    protected void setAlarmState(AlarmState newState) {
+        state = newState;
+        if (newState == AlarmState.ALARM_DISABLED) {
+            disableWithColor();
+        } else if (triggeredAlarms == 0) {
+            triggerWithColor();
+        } else {
+            enableWithColor();
+        }
     }
+    
     /**
-     * Called when the observed object updates.	
+     * Called when the observed object updates.
+     * 
      * @param observable The observed object.
      * @param object The new value.
      */
-    public final void update (java.util.Observable observable, Object object) {
-        throw new RuntimeException("Not implemented!");
+    public void update(Observable observable, Object object) {
+        TankAlarm<?> alarm = (TankAlarm<?>) observable;
+        if (alarm.isAlarmTriggered()) {
+            triggeredAlarms++;
+        } else {
+            // It's assumed that the light receives a notification about an alarm that isn't triggered
+            // after this alarm was triggered. 
+            triggeredAlarms--;
+        }
+        if (state == AlarmState.ALARM_ENABLED) {
+            if (triggeredAlarms == 0) {
+                enableWithColor();
+            } else {
+                triggerWithColor();
+            }
+        }
+    }
+    
+    /**
+     * Disables the light visually with changing the colors.
+     */
+    private void disableWithColor() {
+        redLight.setStroke(MonitoringViewConstants.ALARM_DISABLED);
+        redLight.setFill(Color.TRANSPARENT);
+        greenLight.setStroke(MonitoringViewConstants.ALARM_DISABLED);
+        greenLight.setFill(Color.TRANSPARENT);
+    }
+    
+    /**
+     * Enables the light visually with changing the colors.
+     */
+    private void enableWithColor() {
+        redLight.setStroke(MonitoringViewConstants.ALARM_TRIGGERED);
+        redLight.setFill(Color.TRANSPARENT);
+        greenLight.setStroke(MonitoringViewConstants.ALARM_ENABLED);
+        greenLight.setFill(MonitoringViewConstants.ALARM_ENABLED);
+    }
+    
+    /**
+     * Shows visually that at least one alarm is triggered.
+     */
+    private void triggerWithColor() {
+        redLight.setStroke(MonitoringViewConstants.ALARM_TRIGGERED);
+        redLight.setFill(MonitoringViewConstants.ALARM_TRIGGERED);
+        greenLight.setStroke(MonitoringViewConstants.ALARM_ENABLED);
+        greenLight.setFill(Color.TRANSPARENT);
     }
 }
