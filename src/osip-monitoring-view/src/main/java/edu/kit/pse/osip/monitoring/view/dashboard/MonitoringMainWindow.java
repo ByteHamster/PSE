9f6aec2f5deb4e508a8fa7918020ed5621307c1e
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-11 01:46:25 +0100

    [monitor-dashboard] Implemented the MonitoringMainWindow.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/MonitoringMainWindow.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/dashboard/MonitoringMainWindow.java
@@ -1,36 +1,122 @@
 package edu.kit.pse.osip.monitoring.view.dashboard;
 
+import edu.kit.pse.osip.core.model.base.ProductionSite;
 import edu.kit.pse.osip.core.model.base.TankSelector;
+import edu.kit.pse.osip.core.utils.language.Translator;
+import java.util.EnumMap;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 /**
  * The entry point for the monitoring view. It shows all sensor datas graphically.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class MonitoringMainWindow {
-    public MixTankVisualization mixTank;
-    public TankVisualization tanks;
-    public Light light;
-    public LoggingConsole log;
-    public MonitoringMenu menu;
+final class MonitoringMainWindow {
+    /**
+     * The menu bar for the monitoring.
+     */
+    private MonitoringMenu menu;
+    
+    /**
+     * The visualizations for the tanks.
+     */
+    private EnumMap<TankSelector, TankVisualization> tanks;
+    
+    /**
+     * The visualization for the mixtank.
+     */
+    private MixTankVisualization mixTank;
+    
+    /**
+     * The logging console.
+     */
+    private LoggingConsole log;
+    
+    /**
+     * The traffic light to show the over all status of the alarms.
+     */
+    private Light light;
+    
     /**
      * Initializes the window.
+     * 
      * @param primaryStage The primary window.
+     * @param currentModel The current model used to initialize all observable objects and initial values.
      */
-    protected MonitoringMainWindow (javafx.stage.Stage primaryStage) {
-        throw new RuntimeException("Not implemented!");
+    protected MonitoringMainWindow(Stage primaryStage, ProductionSite currentModel) {
+        menu = new MonitoringMenu();
+        tanks = new EnumMap<TankSelector, TankVisualization>(TankSelector.class);
+        for (TankSelector selector : TankSelector.valuesWithoutMix()) {
+            tanks.put(selector, new TankVisualization(currentModel.getUpperTank(selector)));
+        }
+        mixTank = new MixTankVisualization(currentModel.getMixTank());
+        log = new LoggingConsole();
+        light = new Light();
+        makeLayout(primaryStage);
     }
+    
+    /**
+     * Does all relevant layout things.
+     * 
+     * @param primaryStage stage on which all elements will be shown.
+     */
+    private void makeLayout(Stage primaryStage) {
+        BorderPane mainPane = new BorderPane();
+        mainPane.setStyle("-fx-font-size: " + MonitoringViewConstants.FONT_SIZE + "px;");
+        mainPane.setTop(menu);
+        
+        GridPane tankPane = new GridPane();
+        tankPane.setVgap(MonitoringViewConstants.ELEMENTS_GAP);
+        tankPane.setHgap(MonitoringViewConstants.ELEMENTS_GAP);
+        tankPane.setPadding(new Insets(MonitoringViewConstants.ELEMENTS_GAP));
+        int currentColumn = 0;
+        for (TankVisualization tank : tanks.values()) {
+            tankPane.add(tank, currentColumn, 0);
+            currentColumn++;
+        }
+        tankPane.add(mixTank, currentColumn, 0);
+        mainPane.setCenter(tankPane);
+        
+        HBox hbox = new HBox();
+        hbox.setSpacing(MonitoringViewConstants.ELEMENTS_GAP);
+        hbox.setPadding(new Insets(MonitoringViewConstants.ELEMENTS_GAP));
+        hbox.getChildren().addAll(log, light);
+        mainPane.setBottom(hbox);
+        
+        Scene scene = new Scene(mainPane);
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle(Translator.getInstance().getString("monitoring.title"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        log.setPrefWidth(0.95 * mainPane.getWidth());
+        log.setPrefHeight(0.2 * mainPane.getHeight());
+        light.setPrefWidth(0.05 * mainPane.getWidth());
+        light.setPrefHeight(log.getPrefHeight());
+    }
+    
     /**
      * Returns the current used visualization for a specified tank.
+     * 
      * @return The current used visualization of a specified tank.
      * @param tank The tank whose current used visualization should be returned.
      */
-    protected final AbstractTankVisualization getTank (TankSelector tank) {
-        throw new RuntimeException("Not implemented!");
+    protected AbstractTankVisualization getTank(TankSelector tank) {
+        return tanks.get(tank);
     }
+    
     /**
      * Returns the current used menu for the monitoring view.
+     * 
      * @return the current used menu for the monitoring view.
      */
-    protected final MonitoringMenu getMonitoringMenu () {
-        throw new RuntimeException("Not implemented!");
+    protected MonitoringMenu getMonitoringMenu() {
+        return menu;
     }
 }
