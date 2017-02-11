7d2fc2701d97ab4e3a4b150522c8364ade95ea8c
Martin Armbruster <armbrustermartin@googlemail.com>
2017-02-11 23:21:21 +0100

    [monitoring-settings] Implemented the AlarmSettings.

--- src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/settings/AlarmSettings.java
+++ src/osip-monitoring-view/src/main/java/edu/kit/pse/osip/monitoring/view/settings/AlarmSettings.java
@@ -2,64 +2,128 @@ package edu.kit.pse.osip.monitoring.view.settings;
 
 import edu.kit.pse.osip.core.io.files.ClientSettingsWrapper;
 import edu.kit.pse.osip.core.model.base.TankSelector;
+import edu.kit.pse.osip.core.utils.language.Translator;
+import edu.kit.pse.osip.monitoring.view.dashboard.MonitoringViewConstants;
+import java.util.EnumMap;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 /**
  * Contains all controls for enabling / disabling the alarms in the monitoring view.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
  */
-public class AlarmSettings extends javafx.scene.control.ScrollPane {
+final class AlarmSettings extends ScrollPane {
     /**
      * All check boxes for disabling / enabling underflow alarms.
      */
-    private java.util.Collection<javafx.scene.control.CheckBox> underflowEnabled;
+    private EnumMap<TankSelector, CheckBox> underflowEnabled;
+    
     /**
      * Contains all check boxes for disabling / enabling overflow alarms.
      */
-    private java.util.Collection<javafx.scene.control.CheckBox> overflowEnabled;
+    private EnumMap<TankSelector, CheckBox> overflowEnabled;
+    
     /**
-     * All check boxes for disabling / enabling overflow alarms for temperatures.
+     * All check boxes for disabling / enabling overheating alarms for temperatures.
      */
-    private java.util.Collection<javafx.scene.control.CheckBox> temperatureOverflowEnabled;
+    private EnumMap<TankSelector, CheckBox> temperatureOverheatingEnabled;
+    
     /**
-     * All check boxes for disabling / enabling the underflow alarms for the temperature.
+     * All check boxes for disabling / enabling the undercooling alarms for the temperature.
      */
-    private java.util.Collection<javafx.scene.control.CheckBox> temperatureUnderflowEnabled;
+    private EnumMap<TankSelector, CheckBox> temperatureUndercoolingEnabled;
+    
     /**
      * Creates and initializes all controls regarding alarms for the settings.
+     * 
      * @param currentSettings The current settings used to display them.
      */
-    protected AlarmSettings (ClientSettingsWrapper currentSettings) {
-        throw new RuntimeException("Not implemented!");
+    protected AlarmSettings(ClientSettingsWrapper currentSettings) {
+        underflowEnabled = new EnumMap<TankSelector, CheckBox>(TankSelector.class);
+        overflowEnabled = new EnumMap<TankSelector, CheckBox>(TankSelector.class);
+        temperatureOverheatingEnabled = new EnumMap<TankSelector, CheckBox>(TankSelector.class);
+        temperatureUndercoolingEnabled = new EnumMap<TankSelector, CheckBox>(TankSelector.class);
+        createLayout(currentSettings);
     }
+    
+    /**
+     * Creates the layout for this element.
+     * 
+     * @param currentSettings The current settings.
+     */
+    private void createLayout(ClientSettingsWrapper currentSettings) {
+        Translator translator = Translator.getInstance();
+        VBox layout = new VBox(MonitoringViewConstants.ELEMENTS_GAP);
+        CheckBox currentBox;
+        for (TankSelector tank : TankSelector.values()) {
+            currentBox = new CheckBox(String.format(translator.getString("monitoring.settings.alarms.liquidUnderflow"),
+                    tank.toString()));
+            currentBox.setSelected(currentSettings.getUnderflowAlarm(tank, true));
+            underflowEnabled.put(tank, currentBox);
+            layout.getChildren().add(currentBox);
+            currentBox = new CheckBox(String.format(translator.getString("monitoring.settings.alarms.liquidOverflow"),
+                    tank.toString()));
+            currentBox.setSelected(currentSettings.getOverflowAlarm(tank, true));
+            overflowEnabled.put(tank, currentBox);
+            layout.getChildren().add(currentBox);
+            currentBox = new CheckBox(String.format(
+                    translator.getString("monitoring.settings.alarms.temperatureOverheating"), tank.toString()));
+//            currentBox.setSelected(currentSettings.getTempOverheatAlarm(tank, true));
+            temperatureOverheatingEnabled.put(tank, currentBox);
+            layout.getChildren().add(currentBox);
+            currentBox = new CheckBox(String.format(
+                    translator.getString("monitoring.settings.alarms.temperatureUndercooling"), tank.toString()));
+//            currentBox.setSelected(currentSettings.getTempUndercoolAlarm(tank, true));
+            temperatureUndercoolingEnabled.put(tank, currentBox);
+            layout.getChildren().add(currentBox);
+        }
+        this.setContent(layout);
+    }
+    
     /**
      * Returns true if the underflow alarm for a specified tank should be enabled and false otherwise.
+     * 
      * @return true if the underflow alarm for a specified tank should be enabled and false otherwise.
      * @param tank Tank for which should be looked up if the underflow alarm should be enabled or not.
      */
-    protected final boolean isUnderflowEnabled (TankSelector tank) {
-        throw new RuntimeException("Not implemented!");
+    protected boolean isUnderflowEnabled(TankSelector tank) {
+        return underflowEnabled.get(tank).isSelected();
     }
+    
     /**
      * Returns true if the overflow alarm for a specified tank should be enabled and false otherwise.
+     * 
      * @return true if the overflow alarm for a specified tank should be enabled and false otherwise.
      * @param tank Tank for which should be looked up if the overflow alarm should be enabled or not.
      */
-    protected final boolean isOverflowEnabled (TankSelector tank) {
-        throw new RuntimeException("Not implemented!");
+    protected boolean isOverflowEnabled(TankSelector tank) {
+        return overflowEnabled.get(tank).isSelected();
     }
+    
     /**
-     * Returns true if the overflow alarm for the temperature for a specified tank should be enabled and false otherwise.
-     * @return true if the overflow alarm for the temperature for a specified tank should be enabled. false otherwise.
+     * Returns true if the overheating alarm for the temperature for a specified tank should be enabled
+     * and false otherwise.
+     * 
+     * @return true if the overheating alarm for the temperature for a specified tank should be enabled.
+     * false otherwise.
      * @param tank The tank whose value for the temperature overflow alarm will be returned.
      */
-    protected final boolean isTemperatureOverflowEnabled (TankSelector tank) {
-        throw new RuntimeException("Not implemented!");
+    protected boolean isTemperatureOverheatingEnabled(TankSelector tank) {
+        return temperatureOverheatingEnabled.get(tank).isSelected();
     }
+    
     /**
-     * Returns true if the underflow alarm for the temperature for a specified tank should be enabled and false otherwise.
-     * @return true if the overflow alarm for the temperature for a specified tank should be enabled. false otherwise.
+     * Returns true if the undercooling alarm for the temperature for a specified tank should be enabled
+     * and false otherwise.
+     * 
+     * @return true if the undercooling alarm for the temperature for a specified tank should be enabled.
+     * false otherwise.
      * @param tank The tank whose value for enabling / disabling the temperature underflow alarm will be returned.
      */
-    protected final boolean isTemperatureUnderflowEnabled (TankSelector tank) {
-        throw new RuntimeException("Not implemented!");
+    protected boolean isTemperatureUndercoolingEnabled(TankSelector tank) {
+        return temperatureUndercoolingEnabled.get(tank).isSelected();
     }
 }
