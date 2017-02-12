5e025fc5797e078c9462d64048d22cc214ecb717
ByteHamster <info@bytehamster.com>
2017-02-12 13:47:34 +0100

    [monitoring/controller] Implemented TankClients

--- src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MixTankClient.java
+++ src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/MixTankClient.java
@@ -1,26 +1,33 @@
 package edu.kit.pse.osip.monitoring.controller;
 
 import edu.kit.pse.osip.core.io.networking.RemoteMachine;
+import edu.kit.pse.osip.core.io.networking.UAIdentifiers;
 import edu.kit.pse.osip.core.opcua.client.FloatReceivedListener;
+import edu.kit.pse.osip.core.opcua.client.UAClientException;
 
 /**
  * Client for reading the values of a mixtank. Contains a motor.
+ *
+ * @author Hans-Peter Lehmann
+ * @version 1.0
  */
-public class MixTankClient extends edu.kit.pse.osip.monitoring.controller.AbstractTankClient {
+public class MixTankClient extends AbstractTankClient {
     /**
      * Creates a new OPC UA client to allow reading the values of a mixtank
      * @param machine The machine to connect to
      */
-    public MixTankClient (RemoteMachine machine) {
-        super("", "");
-        throw new RuntimeException("Not implemented!");
+    public MixTankClient(RemoteMachine machine) {
+        super(machine, UAIdentifiers.TANK_MIX);
     }
+
     /**
-     * Subscribes the motor speed of the tank at the given interval. Can be called again with the same listener to change interval.
+     * Subscribes the motor speed of the tank at the given interval.
+     * Can be called again with the same listener to change interval.
      * @param interval The interval to use when subscribing to the value
      * @param listener The callback function to be called as soon as the subscribed value was received
+     * @throws UAClientException If adding the subscription fails
      */
-    public final void subscribeMotorSpeed (int interval, FloatReceivedListener listener) {
-        throw new RuntimeException("Not implemented!");
+    public void subscribeMotorSpeed(int interval, FloatReceivedListener listener) throws UAClientException {
+        subscribeFloat(UAIdentifiers.MOTOR_RPM, interval, listener);
     }
 }
--- src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/TankClient.java
+++ src/osip-monitoring-controller/src/main/java/edu/kit/pse/osip/monitoring/controller/TankClient.java
@@ -1,26 +1,33 @@
 package edu.kit.pse.osip.monitoring.controller;
 
 import edu.kit.pse.osip.core.io.networking.RemoteMachine;
+import edu.kit.pse.osip.core.io.networking.UAIdentifiers;
 import edu.kit.pse.osip.core.opcua.client.FloatReceivedListener;
+import edu.kit.pse.osip.core.opcua.client.UAClientException;
 
 /**
  * Client for receiving information about the upper tanks. Contains additional valve.
+ *
+ * @author Hans-Peter Lehmann
+ * @version 1.0
  */
-public class TankClient extends edu.kit.pse.osip.monitoring.controller.AbstractTankClient {
+public class TankClient extends AbstractTankClient {
     /**
      * Creates a new OPC UA client to allow reading the values of an input tank
      * @param machine The machine to connnect to
      */
-    public TankClient (RemoteMachine machine) {
-        super("", "");
-        throw new RuntimeException("Not implemented!");
+    public TankClient(RemoteMachine machine) {
+        super(machine, UAIdentifiers.TANK);
     }
+
     /**
-     * Subscribes the input flow rate of the tank at the given interval. Can be called again with the same listener to change interval.
+     * Subscribes the input flow rate of the tank at the given interval.
+     * Can be called again with the same listener to change interval.
      * @param interval The interval to use when subscribing to the value
      * @param listener The callback function to be called as soon as the subscribed value was received
+     * @throws UAClientException If adding the subscription fails
      */
-    public final void subscribeInputFlowRate (int interval, FloatReceivedListener listener) {
-        throw new RuntimeException("Not implemented!");
+    public void subscribeInputFlowRate(int interval, FloatReceivedListener listener) throws UAClientException {
+        subscribeFloat(UAIdentifiers.PIPE_IN_FLOW_RATE, interval, listener);
     }
 }
