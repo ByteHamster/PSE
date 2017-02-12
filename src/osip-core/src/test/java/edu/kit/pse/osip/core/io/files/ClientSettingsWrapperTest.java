fafd55663898cd12df4c2ef6c42fcf4b7f5ecc8d
marimbMax <stick@nurfuerspam.de>
2017-02-12 15:17:20 +0100

    [core/io/files] Changed output of ClientSettingsWrapper und made new tests for it

--- src/osip-core/src/test/java/edu/kit/pse/osip/core/io/files/ClientSettingsWrapperTest.java
+++ src/osip-core/src/test/java/edu/kit/pse/osip/core/io/files/ClientSettingsWrapperTest.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,107 +24,157 @@ public class ClientSettingsWrapperTest {
     URL url;
     File propertiesFile;
     
+    /**
+     * Set up for tests
+     * @throws Exception Exceptions in set up
+     */
     @Before
     public void setUp() throws Exception {
         url = Thread.currentThread().getContextClassLoader().getResource("testClientSettings.properties");
         propertiesFile = new File(url.getPath());
         wrapper = new ClientSettingsWrapper(propertiesFile);
     }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    
+    /**
+     * Test setting ports
+     */
     @Test
     public void testSetServerPort() {
         wrapper.setServerPort(TankSelector.MAGENTA, 1042);        
         assertEquals(wrapper.getPort(TankSelector.MAGENTA), 1042);
     }
 
+    /**
+     * Test setting fetchinterval
+     */
     @Test
     public void testSetFetchInterval() {
         wrapper.setFetchInterval(52);        
         assertEquals(wrapper.getFetchInterval(), 52);
     }
     
+    /**
+     * Test setting overflow alarm status
+     */
     @Test
     public void testSetOverflowAlarm() {
         wrapper.setOverflowAlarm(TankSelector.YELLOW, true);        
         assertEquals(wrapper.getOverflowAlarm(TankSelector.YELLOW), true);
     }
     
+    /**
+     * Test setting temp diagram status
+     */
     @Test
     public void testSetTempDiagram() {
         wrapper.setTempDiagram(TankSelector.YELLOW, true);        
         assertEquals(wrapper.getTempDiagram(TankSelector.YELLOW), true);
     }
     
+    /**
+     * test setting server hostname
+     */
     @Test
     public void testSetServerHostname() {
         wrapper.setServerHostname(TankSelector.MAGENTA, "123.123");        
         assertEquals(wrapper.getHostname(TankSelector.MAGENTA), "123.123");
     }
     
+    /**
+     * Test setting underflow alarm status
+     */
     @Test
     public void testSetUnderflowAlarm() {
         wrapper.setUnderflowAlarm(TankSelector.YELLOW, true);        
         assertEquals(wrapper.getUnderflowAlarm(TankSelector.YELLOW), true);
     }
     
+    /**
+     * Test setting fill level diagram status
+     */
     @Test
     public void testSetFillLevelDiagram() {
         wrapper.setFillLevelDiagram(TankSelector.YELLOW, true);        
         assertEquals(wrapper.getFillLevelDiagram(TankSelector.YELLOW), true);
     }
     
+    /**
+     * Test getting fetchinterval
+     */
     @Test
     public void testGetFetchInterval() {
         int result = wrapper.getFetchInterval();
         assertEquals(result, 80);
     }
     
+    /**
+     * Test getting overflow alarm status
+     */
     @Test
     public void testGetOverflowAlarm() {
         boolean result = wrapper.getOverflowAlarm(TankSelector.YELLOW);
         assertEquals(result, false);
     }
     
+    /**
+     * Test getting underflow alarm status
+     */
     @Test
-    public void testGetUnderFlowAlarm() {
+    public void testGetUnderflowAlarm() {
         boolean result = wrapper.getUnderflowAlarm(TankSelector.YELLOW);
         assertEquals(result, false);
     }
     
+    /**
+     * Test getting temp diagram status
+     */
     @Test
     public void testGetTempDiagram() {
         boolean result = wrapper.getTempDiagram(TankSelector.YELLOW);
         assertEquals(result, false);
     }
     
+    /**
+     * Test getting fill level diagram status
+     */
     @Test
     public void testGetFillLevelDiagram() {
         boolean result = wrapper.getFillLevelDiagram(TankSelector.YELLOW);
         assertEquals(result, false);
     }
     
+    /**
+     * Test getting hostname
+     */
     @Test
     public void testGetHostname() {
         String result = wrapper.getHostname(TankSelector.MAGENTA);
         assertEquals(result, "hostNAME");
     }
     
+    /**
+     * Test getting port
+     */
     @Test
     public void testGetPort() {
         int result = wrapper.getPort(TankSelector.MAGENTA);
         assertEquals(result, 4242);
     }
     
+    /**
+     * Test saving to file
+     * @throws FileNotFoundException Exception in file param
+     * @throws IOException Exception in IO stream
+     */
     @Test
     public void testSaveSettings() throws FileNotFoundException, IOException {
         wrapper.setServerPort(TankSelector.MAGENTA, 424299);
         wrapper.saveSettings();
-        wrapper = new ClientSettingsWrapper(propertiesFile);
-        assertEquals(wrapper.getPort(TankSelector.YELLOW), 424299);
+        ClientSettingsWrapper helpWrapper = new ClientSettingsWrapper(propertiesFile);
+        int helpPort = helpWrapper.getPort(TankSelector.MAGENTA);
+        wrapper.setServerPort(TankSelector.MAGENTA, 4242);
+        wrapper.saveSettings();
+        assertEquals(helpPort, 424299);
     }
     
 }
