7c0661b5099c05f4075a293ccfda54bf1f207a0c
marimbMax <stick@nurfuerspam.de>
2017-02-14 17:19:10 +0100

    [core/model/behavior] Cleared checkstyle violations in TankAlarm

--- src/osip-model/src/main/java/edu/kit/pse/osip/core/model/behavior/TankAlarm.java
+++ src/osip-model/src/main/java/edu/kit/pse/osip/core/model/behavior/TankAlarm.java
@@ -7,9 +7,10 @@ import edu.kit.pse.osip.core.model.base.Liquid;
 
 /**
  * An abstract class which monitors some attribute of an AbstractTank.
+ * @param <T> The type of notified value 
  */
 public abstract class TankAlarm <T extends Comparable<T>> extends java.util.Observable implements java.util.Observer {
-    public AbstractTank tank;
+    protected AbstractTank tank;
     private T threshold;
     private AlarmBehavior behavior;
     private boolean triggered = false;
@@ -21,22 +22,23 @@ public abstract class TankAlarm <T extends Comparable<T>> extends java.util.Obse
      * @param threshold The threshold of the value.
      * @param behavior Whether the alarm should trigger if the value is bigger or smaller than the threshold.
      */
-    public TankAlarm (AbstractTank tank, T threshold, AlarmBehavior behavior) {
+    public TankAlarm(AbstractTank tank, T threshold, AlarmBehavior behavior) {
         this.tank = tank;
         this.threshold = threshold;
         this.behavior = behavior;
     }
     /**
      * Get the value of the tank, which is monitored by the alarm. This should be overriden by subclasses.
+     * @return notified value
      */
-    protected abstract T getNotifiedValue ();
+    protected abstract T getNotifiedValue();
     /**
      * The method called by the tank if something changes.
      * @param observable the observerable object.
      * @param object The changed value.
      */
-    public final void update (Observable observable, Object object) {
-        assert(object instanceof Liquid);
+    public final void update(Observable observable, Object object) {
+        assert (object instanceof Liquid);
         liquid = (Liquid) object;
         if (behavior == AlarmBehavior.GREATER_THAN) {
             if (this.getNotifiedValue().compareTo(threshold) > 0) {
@@ -53,7 +55,7 @@ public abstract class TankAlarm <T extends Comparable<T>> extends java.util.Obse
      * Return whether the alarm is active at the moment.
      * @return true if the alarm is currently triggered.
      */
-    public final boolean isAlarmTriggered () {
+    public final boolean isAlarmTriggered() {
         return triggered;
     }
 }
