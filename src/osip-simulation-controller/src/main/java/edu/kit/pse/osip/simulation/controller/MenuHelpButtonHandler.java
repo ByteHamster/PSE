2f230251904d87957e5da2677a9e46310a6de8aa
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-16 00:03:06 +0100

    [simulation.controller] Make ButtonHandlers implement EventHandler.

--- src/osip-simulation-controller/src/main/java/edu/kit/pse/osip/simulation/controller/MenuHelpButtonHandler.java
+++ src/osip-simulation-controller/src/main/java/edu/kit/pse/osip/simulation/controller/MenuHelpButtonHandler.java
@@ -2,11 +2,12 @@ package edu.kit.pse.osip.simulation.controller;
 
 import edu.kit.pse.osip.simulation.view.dialogs.HelpDialog;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 /**
  * Handles a click on the help menu button in the simulation view.
  */
-public class MenuHelpButtonHandler {
+public class MenuHelpButtonHandler implements EventHandler<ActionEvent> {
     /**
      * Handles a click on the help menu button in the simulation view.
      * @param event The ocurred event.
