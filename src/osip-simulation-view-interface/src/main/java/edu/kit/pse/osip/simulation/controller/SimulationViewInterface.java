75ec4d0e045a24bdb7362bd1cbcea53f9e45fb87
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-16 17:57:12 +0100

    [simulation.view] Make SimulatioViewInterface prettier.

--- src/osip-simulation-view-interface/src/main/java/edu/kit/pse/osip/simulation/controller/SimulationViewInterface.java
+++ src/osip-simulation-view-interface/src/main/java/edu/kit/pse/osip/simulation/controller/SimulationViewInterface.java
@@ -1,16 +1,20 @@
 package edu.kit.pse.osip.simulation.controller;
 
+import javafx.stage.Stage;
+
 /**
  * Provides abstraction from the view
  */
 public interface SimulationViewInterface {
+
     /**
      * Draw the simulation view to the stage
      * @param primaryStage The stage that is provided by JavaFx
      */
-    public void start (javafx.stage.Stage primaryStage);
+    public void start(Stage primaryStage);
+
     /**
      * The simulation is replaced by the OverflowOverlay.
      */
-    public void showOverflow ();
+    public void showOverflow();
 }
