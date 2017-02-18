0a0228cd7549191394ddaf31572d63d2f56848d4
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-18 17:11:08 +0100

    [simulation.view.control] Have AbstractTankTab extend Tab instead of TabPane.

--- src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/control/AbstractTankTab.java
+++ src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/control/AbstractTankTab.java
@@ -1,11 +1,11 @@
 package edu.kit.pse.osip.simulation.view.control;
 
-import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
 
 /**
  * This class contains the controls for a single tank in the simulation.
  */
-public abstract class AbstractTankTab extends TabPane {
+public abstract class AbstractTankTab extends Tab {
 
     /**
      * Gets the value of outFlow.
