4bfb4473a3f22e87b50e2a76d221c36ed30166f2
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-22 15:55:08 +0100

    [simulation-control] Change simulation control interfaces.

---
+++ src/osip-simulation-view-interface/src/main/java/edu/kit/pse/osip/simulation/controller/SimulationControlInterface.java
@@ -0,0 +1,28 @@
+package edu.kit.pse.osip.simulation.controller;
+
+import edu.kit.pse.osip.core.model.base.Pipe;
+import edu.kit.pse.osip.core.model.base.TankSelector;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * Provides abstraction from the view
+ */
+public interface SimulationControlInterface {
+    /**
+     * Sets the listener that is notified of changes to valve thresholds.
+     * @param listener The Consumer that gets all changes to valve thresholds.
+     */
+    void setValveListener(BiConsumer<Pipe, Byte> listener);
+    /**
+     * Sets the listener that is notified of changes in the temperature.
+     * @param listener The Consumer that gets all changes to Tank temperatures
+     */
+    void setTemperatureListener(BiConsumer<TankSelector, Float> listener);
+    /**
+     * Sets the listener that is notified of changes in motor speed.
+     * @param listener The Consumer that gets all to the motorSpeed
+     */
+    void setMotorListener(Consumer<Integer> listener);
+}
