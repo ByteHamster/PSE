1e19edda5de1e0072c8274e45e3b86ac2b18850f
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-02-22 16:56:26 +0100

    [simulation.control] Make the rebase work.

--- src/osip-simulation-view-interface/src/main/java/edu/kit/pse/osip/simulation/controller/SimulationControlInterface.java
+++ src/osip-simulation-view-interface/src/main/java/edu/kit/pse/osip/simulation/controller/SimulationControlInterface.java
@@ -1,48 +1,27 @@
 package edu.kit.pse.osip.simulation.controller;
 
+import edu.kit.pse.osip.core.model.base.Pipe;
 import edu.kit.pse.osip.core.model.base.TankSelector;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
  * Provides abstraction from the view
  */
 public interface SimulationControlInterface {
     /**
-     * Gets the value of inFlow of tank.
-     * @return The value of inFlow of tank
-     * @param tank The tank to get the inflow from
+     * Sets the listener that is notified of changes to valve thresholds.
+     * @param listener The Consumer that gets all changes to valve thresholds.
      */
-    public int getInFlow (TankSelector tank);
+    void setValveListener(BiConsumer<Pipe, Byte> listener);
     /**
-     * 		Gets the value of outFlow of the tank.
-     * @return The value of outFlow of the tank
-     * @param tank The tank to get the outflow from
+     * Sets the listener that is notified of changes in the temperature.
+     * @param listener The Consumer that gets all changes to Tank temperatures
      */
-    public int getOutFlow (TankSelector tank);
-    /**
-     * Gets the value of temp of the tank
-     * @return The value of temp the tank.
-     * @param tank The tank to get the temperature from
-     */
-    public int getTemperature (TankSelector tank);
-    /**
-     * Returns the motor speed of the mixtank
-     * 
-     * @return the motor speed of the mixtank
-     */
-    public int getMotorSpeed ();
-    /**
-     * Sets the listener that is notified of changes in the flow rate. Listner gets the TankSelector of the actually modified tank
-     * @param listener The listener to be called on changes
-     */
-    public void setFlowRateListener (FlowRateListener listener);
-    /**
-     * Sets the listener that is notified of changes in the temperature. Listner gets the TankSelector of the actually modified tank
-     * @param listener The listener to be called on changes
-     */
-    public void setTemperatureListener (TemperatureListener listener);
+    void setTemperatureListener(BiConsumer<TankSelector, Float> listener);
     /**
      * Sets the listener that is notified of changes in motor speed.
-     * @param listener The listener to be called on changes
+     * @param listener The Consumer that gets all to the motorSpeed
      */
-    public void setMotorListener (MotorListener listener);
+    void setMotorListener(Consumer<Integer> listener);
 }
