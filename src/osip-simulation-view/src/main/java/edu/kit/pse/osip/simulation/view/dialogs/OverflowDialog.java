db6b44538a949b12e96a56df590992255eed720b
marimbMax <stick@nurfuerspam.de>
2017-02-27 23:01:03 +0100

    [simulation/view/dialog] Fixed checkstyle issues

--- src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/dialogs/OverflowDialog.java
+++ src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/dialogs/OverflowDialog.java
@@ -29,11 +29,11 @@ import javafx.event.EventHandler;
 public class OverflowDialog extends Stage {
     private TankSelector tank;
     private Button resetButton;
-    private EventHandler<ActionEvent> eventHandler;
+    private EventHandler<ActionEvent> resetButtonHandler;
     private static final int FONT_SIZE = ViewConstants.FONT_SIZE;
     private static final double MIN_WINDOW_HEIGHT = 250;
     private static final double MIN_WINDOW_WIDTH = 500;
-    
+
     /**
      * Constructor of OverflowDialog
      * @param tank Tank which has an overflow
@@ -41,7 +41,7 @@ public class OverflowDialog extends Stage {
     public OverflowDialog(TankSelector tank) {
         this.setTank(tank);
     }
-        
+
     /**
      * Set the tank which has an overflow
      * @param tank The tank which has an overflow
@@ -54,30 +54,34 @@ public class OverflowDialog extends Stage {
         configure();
     }
 
-    public void setResetButtonHandler(EventHandler<ActionEvent> event) {
-        this.eventHandler = event;
+    /**
+     * Setter method for resetButtonHandler
+     * @param eventHandler event handler for reset button
+     */
+    public void setResetButtonHandler(EventHandler<ActionEvent> eventHandler) {
+        resetButtonHandler = eventHandler;
     }
-    
+
     private Text getTextWithTankName(String id, TankSelector tank, Font font) {
         Text text = new Text();
         if (tank != null) {
             text.setText(Translator.getInstance().getString(id) + " " + tank.toString() + "!" + "\n");
         } else {
             throw new IllegalStateException("No tank set. Please call setTank() before calling show()");
-        }               
+        }
         if (font != null) {
             text.setFont(font);
         }
         return text;
     }
-    
+
     private void configure() {
         this.setTitle(Translator.getInstance().getString("simulation.overflowdialog.title"));
         Image imageIcon = new Image(getClass().getClassLoader().getResourceAsStream("icon.png"));
         this.getIcons().add(imageIcon);
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(ViewConstants.ELEMENTS_GAP));
-        
+
         TextFlow overflowTextFlow = new TextFlow();
         Text overflowText = getTextWithTankName("simulation.overflowdialog.overflowmessage", tank,
                 new Font(FONT_SIZE * 3));
@@ -85,24 +89,27 @@ public class OverflowDialog extends Stage {
         overflowTextFlow.getChildren().add(overflowText);
         GridPane.setConstraints(overflowTextFlow, 0, 0);
         GridPane.setValignment(overflowTextFlow, VPos.CENTER);
-        GridPane.setHgrow(overflowTextFlow, Priority.ALWAYS);       
-        
+        GridPane.setHgrow(overflowTextFlow, Priority.ALWAYS);
+
         resetButton = new Button(Translator.getInstance().getString("simulation.overflowdialog.reset"));
         resetButton.setStyle("-fx-font-size: " + ViewConstants.FONT_SIZE * 2 + "px;");
-        resetButton.setDefaultButton(true);  
-        resetButton.setOnAction(event -> {if (eventHandler != null) {
-            eventHandler.handle(event);};
-            this.close();});
-        
+        resetButton.setDefaultButton(true);
+        resetButton.setOnAction(event -> {
+            if (resetButtonHandler != null) {
+                resetButtonHandler.handle(event);
+            }
+            this.close();
+        });
+
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(grid);
         borderPane.setBottom(resetButton);
         borderPane.setPadding(new Insets(0, 0, ViewConstants.ELEMENTS_GAP, 0));
         BorderPane.setAlignment(resetButton, Pos.CENTER);
-        
+
         grid.getChildren().addAll(overflowTextFlow);
-        Scene scene = new Scene(borderPane, MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT);    
-        
+        Scene scene = new Scene(borderPane, MIN_WINDOW_WIDTH, MIN_WINDOW_HEIGHT);
+
         this.setScene(scene);
         this.setMinWidth(MIN_WINDOW_WIDTH);
         this.setMinHeight(MIN_WINDOW_HEIGHT);
