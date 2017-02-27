c1617a94190ca3a2009880a5f419d5da0e08842e
marimbMax <stick@nurfuerspam.de>
2017-02-27 23:15:00 +0100

    qwer

--- src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/dialogs/OverflowDialog.java
+++ src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/dialogs/OverflowDialog.java
@@ -63,8 +63,9 @@ public class OverflowDialog extends Stage {
     private Text getTextWithTankName(String id, TankSelector tank, Font font) {
         Text text = new Text();
         if (tank != null) {
-            text.setText(Translator.getInstance().getString(id) + " " +
-        Translator.getInstance().getString(TankSelector.TRANSLATOR_LABEL_PREFIX + tank.name()) + "!" + "\n");
+            text.setText(Translator.getInstance().getString(id) + " "
+                    + Translator.getInstance().getString(TankSelector.TRANSLATOR_LABEL_PREFIX + tank.name())
+                    + "!" + "\n");
         } else {
             throw new IllegalStateException("No tank set. Please call setTank() before calling show()");
         }
