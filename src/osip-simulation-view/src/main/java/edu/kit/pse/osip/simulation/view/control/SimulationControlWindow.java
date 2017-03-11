cfa6572a066314e69c079c1be212d926521770c8
Niko Wilhelm <niko.wilhelm@gmx.net>
2017-03-11 01:40:13 +0100

    [simulation.view] Remove unused attribute.

--- src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/control/SimulationControlWindow.java
+++ src/osip-simulation-view/src/main/java/edu/kit/pse/osip/simulation/view/control/SimulationControlWindow.java
@@ -26,7 +26,6 @@ import java.util.EnumMap;
 public class SimulationControlWindow extends Stage implements SimulationControlInterface {
 
     private EnumMap<TankSelector, AbstractTankTab> tankTabs;
-    private ProductionSite productionSite;
 
     /**
      * Constructs a new SimulationControlWindow
@@ -34,7 +33,6 @@ public class SimulationControlWindow extends Stage implements SimulationControlI
      */
     public SimulationControlWindow(ProductionSite productionSite) {
         Translator t = Translator.getInstance();
-        this.productionSite = productionSite;
 
         this.getIcons().add(new Image("/icon.png"));
 
