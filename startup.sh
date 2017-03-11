5e36399649649b590741c3bd002ffede30b917fd
David Kahles <david.kahles96@gmail.com>
2017-03-11 10:30:13 +0100

    [docker] Copy dockerfiles to the root directory
    Otherwise we can't COPY the files into the container.
    Also fix the dockerfile name.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xhost +local: $(docker inspect --format='{{ .Config.Hostname }}' docker_simulation)
 xhost +local: $(docker inspect --format='{{ .Config.Hostname }}' docker_monitor)
-docker-compose up
+docker-compose up --build
 xhost -local: $(docker inspect --format='{{ .Config.Hostname }}' docker_simulation)
 xhost -local: $(docker inspect --format='{{ .Config.Hostname }}' docker_monitor)
