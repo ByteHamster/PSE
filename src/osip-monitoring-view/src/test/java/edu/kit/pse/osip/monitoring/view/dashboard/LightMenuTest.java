84c20a5f01aa31e4ef5a163ca8ec5e7e74d3516c
Martin Armbruster <armbrustermartin@googlemail.com>
2017-03-24 00:50:02 +0100

    [monitor-dashboard] Added the LightMenuTest.

---
+++ src/osip-monitoring-view/src/test/java/edu/kit/pse/osip/monitoring/view/dashboard/LightMenuTest.java
@@ -0,0 +1,35 @@
+package edu.kit.pse.osip.monitoring.view.dashboard;
+
+import javafx.stage.Stage;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Test class for the Light and MonitoringMenu classes.
+ * 
+ * @author Martin Armbruster
+ * @version 1.0
+ */
+public class LightMenuTest extends ApplicationTest {
+    @Override
+    public void start(Stage arg0) throws Exception {
+    }
+
+    /**
+     * Tests the Light class's constructor.
+     */
+    @Test
+    public void testLight() {
+        assertNotNull(new Light());
+    }
+    
+    /**
+     * Tests the constructor of the MonitoringMenu class.
+     */
+    @Test
+    public void testMenu() {
+        assertNotNull(new MonitoringMenu());
+    }
+}
