8bf09c69195e89fc96e2c4d77c46c278eb0f0557
ByteHamster <info@bytehamster.com>
2017-03-27 20:45:19 +0200

    [simulation/view] Added test for view callbacks

---
+++ src/osip-simulation-view/src/test/java/edu/kit/pse/osip/simulation/view/main/SimulationViewCallbackTest.java
@@ -0,0 +1,115 @@
+package edu.kit.pse.osip.simulation.view.main;
+
+
+import edu.kit.pse.osip.core.model.base.ProductionSite;
+import edu.kit.pse.osip.core.utils.language.Translator;
+import javafx.stage.Stage;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * @author Hans-Peter Lehmann
+ * @version 1.0
+ */
+public class SimulationViewCallbackTest extends ApplicationTest {
+
+    private SimulationMainWindow mainWindow;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Translator.getInstance().setLocalizationDisabled(true);
+        mainWindow = new SimulationMainWindow(new ProductionSite());
+        mainWindow.start(stage);
+        mainWindow.setProgressIndicatorVisible(false);
+    }
+
+    /**
+     * Resets the translator to show localizations again
+     */
+    @AfterClass
+    public static void resetTranslator() {
+        Translator.getInstance().setLocalizationDisabled(false);
+    }
+
+    /**
+     * Waits for the UI to show up
+     * @throws Exception If test goes wrong
+     */
+    @Before
+    public void waitForUI() throws Exception {
+        Thread.sleep(1000);
+    }
+
+    /**
+     * Test if the settings callback works
+     * @throws Exception If test goes wrong
+     */
+    @Test
+    public void testSettings() throws Exception {
+        CompletableFuture<Boolean> callbackReceived = new CompletableFuture<>();
+        mainWindow.setSettingsButtonHandler(event -> callbackReceived.complete(true));
+        clickOn("simulation.view.menu.file")
+                .clickOn("simulation.view.menu.file.settings");
+        assertTrue(callbackReceived.get(5, TimeUnit.SECONDS));
+    }
+
+    /**
+     * Test if the reset callback works
+     * @throws Exception If test goes wrong
+     */
+    @Test
+    public void testReset() throws Exception {
+        CompletableFuture<Boolean> callbackReceived = new CompletableFuture<>();
+        mainWindow.setResetListener(event -> callbackReceived.complete(true));
+        clickOn("simulation.view.menu.file")
+                .clickOn("simulation.view.menu.file.reset");
+        assertTrue(callbackReceived.get(5, TimeUnit.SECONDS));
+    }
+
+    /**
+     * Test if the about callback works
+     * @throws Exception If test goes wrong
+     */
+    @Test
+    public void testAbout() throws Exception {
+        CompletableFuture<Boolean> callbackReceived = new CompletableFuture<>();
+        mainWindow.setAboutButtonHandler(event -> callbackReceived.complete(true));
+        clickOn("simulation.view.menu.other")
+                .clickOn("simulation.view.menu.other.about");
+        assertTrue(callbackReceived.get(5, TimeUnit.SECONDS));
+    }
+
+    /**
+     * Test if the help callback works
+     * @throws Exception If test goes wrong
+     */
+    @Test
+    public void testHelp() throws Exception {
+        CompletableFuture<Boolean> callbackReceived = new CompletableFuture<>();
+        mainWindow.setHelpButtonHandler(event -> callbackReceived.complete(true));
+        clickOn("simulation.view.menu.other")
+                .clickOn("simulation.view.menu.other.help");
+        assertTrue(callbackReceived.get(5, TimeUnit.SECONDS));
+    }
+
+    /**
+     * Test if the control callback works
+     * @throws Exception If test goes wrong
+     */
+    @Test
+    public void testControl() throws Exception {
+        CompletableFuture<Boolean> callbackReceived = new CompletableFuture<>();
+        mainWindow.setControlButtonHandler(event -> callbackReceived.complete(true));
+        clickOn("simulation.view.menu.view")
+                .clickOn("simulation.view.menu.view.control");
+        assertTrue(callbackReceived.get(5, TimeUnit.SECONDS));
+    }
+}
