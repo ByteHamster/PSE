4274d7e4b34692edbd89634fda7eb1428cf90fa1
David Kahles <david.kahles96@gmail.com>
2017-04-29 18:17:59 +0200

    [docker] Fix startup.sh
    xhost +local doesn't expect a name (it was ignored previously), and the
    provided name didn't make any sense. Update readme to use
    "docker-compose up" per default as long as it works, and use startup.sh
    only if it doesn't.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-xhost +local: $(docker inspect --format='{{ .Config.Hostname }}' docker_simulation)
-xhost +local: $(docker inspect --format='{{ .Config.Hostname }}' docker_monitor)
+xhost +local:
 docker-compose up
-xhost -local: $(docker inspect --format='{{ .Config.Hostname }}' docker_simulation)
-xhost -local: $(docker inspect --format='{{ .Config.Hostname }}' docker_monitor)
+xhost -local:
